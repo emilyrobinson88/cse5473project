0ea9971e6fe2829c3de15a10398b9a61a6568334
leahgillespie <gillespie.267@osu.edu>
2020-11-27 05:58:56 -0500

    Pulled common passwords from databank online, updated seed file with these passwords and how often they've been exposed in a leak

--- pass_app/db/seeds.rb
+++ pass_app/db/seeds.rb
@@ -9,5 +9,49 @@
 #
 # will be using web scraping probably eventually? but for now just this to show how password bank will work
 
-CommonPassword.create(password:'123456', rank:1)
-CommonPassword.create(password:'password', rank:2)
+require 'json'
+require 'mechanize'
+
+#CommonPassword.create(password:'123456', rank:1)
+#CommonPassword.create(password:'password', rank:2)
+
+agent = Mechanize.new
+html_loc = File.dirname(__FILE__)
+html_loc = html_loc + "/../Cache"
+page = agent.get("file:///#{html_loc}/Most common passwords of 2020.html")
+temp = page.search("//div[@id='___gatsby']").children.children.search("//div[@dir='ltr']").search("//main[@class='overflow-hidden cyber-month-offset--grey']").children.children.children
+#table = temp[1]  # gives div above the one I want
+temp2 = temp[2].children.children[0]
+table = temp2.children
+first = true
+table.children.each do |child|
+  if !first
+    data = child.children.children.children
+    count = 0
+    password = ""
+    exposed =""
+    data.children.each do |lowestLevel|
+      lowest = lowestLevel.to_s
+      if lowest.index("span").nil?
+        if count == 0
+          password = lowest
+        elsif count == 3
+          exposed = lowest.delete ","
+          exposed = exposed.to_i
+        end
+        count = count + 1
+      end
+      if count == 4
+        break
+      end
+    end
+    puts password
+    puts exposed
+    #CommonPassword.create(password:password, rank:exposed)
+
+  else
+    first = false
+  end
+
+end
+
