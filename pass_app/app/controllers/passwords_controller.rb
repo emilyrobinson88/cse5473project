f9628da69f990b37c1328356825832379a4b6d64
leahgillespie <gillespie.267@osu.edu>
2020-12-09 00:30:54 -0500

    IP Tracking display works!

--- pass_app/app/controllers/passwords_controller.rb
+++ pass_app/app/controllers/passwords_controller.rb
@@ -1,27 +1,28 @@
 class PasswordsController < ApplicationController
-before_action :set_password, only: [:show, :edit, :update, :destroy]
+	before_action :set_password, only: [:show, :edit, :update, :destroy]
 	def index
 		@passwords=Password.all
 	end
-	
+
 	def show
 		@password=Password.find(params[:id])
 	end
 
 	def new
+		puts "password new"
 		@password=Password.new(:count => 1)
 		@password.save
-		ip = request.remote_ip
-		IpAddress.new(:ipa => ip, :password => @password.pass)
 	end
 
 	def create
+		puts "password create"
 		@password = Password.find_by(allowed_params)
 		ip = request.remote_ip
 		if !@password.nil?
 			currCount = @password[:count]
 			@password.update(:count => currCount + 1)
-			IpAddress.new(:ipa => ip, :password => @password.pass)
+			@ip = IpAddress.new(:ipa => ip, :password => @password.pass)
+			@ip.save
 			respond_to do |format|
 				if @password.save
 					format.html { redirect_to @password }
@@ -34,7 +35,8 @@ before_action :set_password, only: [:show, :edit, :update, :destroy]
 		else
 			@password=Password.new(allowed_params)
 			@password.update(:count => 1)
-			IpAddress.new(:ipa => ip, :password => @password.pass)
+			@ip = IpAddress.new(:ipa => ip, :password => @password.pass)
+			@ip.save
 			respond_to do |format|
 				if @password.save
 					format.html { redirect_to @password }
@@ -44,19 +46,22 @@ before_action :set_password, only: [:show, :edit, :update, :destroy]
 					format.json { render json: @password.errors, status: :unprocessable_entity }
 				end
 			end
-    end
+		end
 	end
 
 	def edit
 		@password = Password.find(params[:id])
-		
+
 	end
 
 	def update
+		puts "password update"
 		respond_to do |format|
-			puts allowed_params
-
 			if @password.update(allowed_params)
+				puts @password.pass
+				ip = request.remote_ip
+				@ip = IpAddress.new(:ipa => ip, :password => @password.pass)
+				@ip.save
 				format.html { redirect_to @password }
 				format.json { render :show, status: :ok, location: @password }
 			else
@@ -68,26 +73,26 @@ before_action :set_password, only: [:show, :edit, :update, :destroy]
 
 	def destroy
 		@password.destroy
-    	respond_to do |format|
-    	  format.html { redirect_to passwords_url, notice: 'password was successfully destroyed.' }
-     	  format.json { head :no_content }
-    end
+		respond_to do |format|
+			format.html { redirect_to passwords_url, notice: 'password was successfully destroyed.' }
+			format.json { head :no_content }
+		end
 	end
-  
- 
 
 
 
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_password
-      @password = Password.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def allowed_params
-      params.require(:password).permit(:pass)
-    end
+
+	private
+	# Use callbacks to share common setup or constraints between actions.
+	def set_password
+		@password = Password.find(params[:id])
+	end
+
+	# Only allow a list of trusted parameters through.
+	def allowed_params
+		params.require(:password).permit(:pass)
+	end
 
 
 end
