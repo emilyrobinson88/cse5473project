40206aff09bb7abd7ee92850d5cdea562ce8ab97
leahgillespie <gillespie.267@osu.edu>
2020-12-10 03:19:11 -0500

    Fixed tracking passwords that are being added to the site

--- pass_app/app/controllers/passwords_controller.rb
+++ pass_app/app/controllers/passwords_controller.rb
@@ -10,7 +10,7 @@ class PasswordsController < ApplicationController
 
 	def new
 		puts "password new"
-		@password=Password.new(:count => 1)
+		@password=Password.new(:count => 0)
 		@password.save
 	end
 
@@ -55,20 +55,42 @@ class PasswordsController < ApplicationController
 	end
 
 	def update
-		puts "password update"
-		respond_to do |format|
-			if @password.update(allowed_params)
-				puts @password.pass
-				ip = request.remote_ip
-				@ip = IpAddress.new(:ipa => ip, :password => @password.pass)
-				@ip.save
+		@password.update(allowed_params)
+		ip = request.remote_ip
+		@ip = IpAddress.new(:ipa => ip, :password => @password.pass)
+		@ip.save
+		matches = Password.where(:pass => @password.pass)
+		puts matches.count
+		if matches.count > 1
+			@password.destroy
+			new_count = matches[0].count + 1
+			matches[0].update_attribute(:count, new_count)
+			respond_to do |format|
+				format.html { redirect_to matches[0] }
+				format.json { render :show, status: :ok, location: matches[0] }
+			end
+		else
+			@password.update_attribute(:count, 1)
+			respond_to do |format|
 				format.html { redirect_to @password }
 				format.json { render :show, status: :ok, location: @password }
-			else
-				format.html { render :edit }
-				format.json { render json: @password.errors, status: :unprocessable_entity }
 			end
 		end
+		# if matches has more than 1, @password.destroy and other match update_attribute(:count, old_count + 1)
+		# puts "password update"
+		# respond_to do |format|
+		# 	if @password.update(allowed_params)
+		# 		puts @password.pass
+		# 		ip = request.remote_ip
+		# 		@ip = IpAddress.new(:ipa => ip, :password => @password.pass)
+		# 		@ip.save
+		# 		format.html { redirect_to @password }
+		# 		format.json { render :show, status: :ok, location: @password }
+		# 	else
+		# 		format.html { render :edit }
+		# 		format.json { render json: @password.errors, status: :unprocessable_entity }
+		# 	end
+		# end
 	end
 
 	def destroy
